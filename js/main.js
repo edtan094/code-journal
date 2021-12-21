3b1ac1c98908a224317cdb5a8239d8d5ccf1d25c
Edwin Tan <etan094@gmail.com>
2021-12-20 17:12:34 -0800

    Put the form's input values into a new object.

--- js/main.js
+++ js/main.js
@@ -7,8 +7,22 @@ var $photoURL = document.querySelector('#photo-URL');
 function photoURLInput(event) {
   // console.log(event.target);
   // console.log(event.target.tagName);
-  $imagePlaceHolder.removeAttribute('src');
   $imagePlaceHolder.setAttribute('src', $photoURL.value);
 }
-
 $photoURL.addEventListener('input', photoURLInput);
+
+var $newEntryButton = document.querySelector('.new-entry-button');
+var $titleValue = document.querySelector('.title-value').value;
+var $photoURLValue = document.querySelector('#photo-URL').value;
+var $notesValue = document.querySelector('.notes-value').value;
+function addingInputIntoObject(event) {
+  var inputs {
+    title:
+    photoURL:
+    notes:
+  }
+  inputs.title = $titleValue
+  inputs.photoURL = $photoURLValue
+  inputs.notes = $notesValue
+}
+$newEntryButton.addEventListener('submit', addingInputIntoObject);
