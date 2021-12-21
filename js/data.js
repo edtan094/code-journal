6ae6332ba3ed14183a320ad4196bbd0316cf978b
Edwin Tan <etan094@gmail.com>
2021-12-20 22:13:53 -0800

    Verify that each new entry receives an incrementing entryId.

--- js/data.js
+++ js/data.js
@@ -7,11 +7,16 @@ var data = {
   nextEntryId: 1
 };
 var previousEntriesJSON = localStorage.getItem('entry');
-if (previousEntriesJSON !== null) {
+var previousNextEntryId = localStorage.getItem('entry ID');
+if (previousEntriesJSON !== null && previousNextEntryId !== 1) {
   data.entries = JSON.parse(previousEntriesJSON);
+  data.nextEntryId = JSON.parse(previousNextEntryId);
 }
 
 window.addEventListener('beforeunload', function () {
   var savedImagesJSON = JSON.stringify(data.entries);
+  var savedNextEntryId = JSON.stringify(data.nextEntryId);
+
   localStorage.setItem('entry', savedImagesJSON);
+  this.localStorage.setItem('entry ID', savedNextEntryId);
 });
