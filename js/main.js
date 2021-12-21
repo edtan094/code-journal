c0436dc248da2083db21f5e803129e9de7735d9f
Edwin Tan <etan094@gmail.com>
2021-12-21 10:33:05 -0800

    Cleaned up and remove console.log in javascript

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 /* global data */
 /* exported data */
 
-// var $imageContainer = document.querySelector('.image-container');
 var $imagePlaceHolder = document.querySelector('.image-place-holder');
 var $photoURL = document.querySelector('#photo-URL');
 function photoURLInput(event) {
-  // console.log(event.target);
-  // console.log(event.target.tagName);
   $imagePlaceHolder.setAttribute('src', $photoURL.value);
 }
 $photoURL.addEventListener('input', photoURLInput);
