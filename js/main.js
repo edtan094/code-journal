1cb14f20d41bfa1955035fe4f5d6d95875ab6d12
Edwin Tan <etan094@gmail.com>
2021-12-20 16:53:59 -0800

    Moved event lister and function cod eto js/main.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 /* global data */
 /* exported data */
+
+var $imageContainer = document.querySelector('.image-container');
+var $imagePlaceHolder = document.querySelector('.image-place-holder');
+var $photoURL = document.querySelector('#photo-URL');
+function photoURLInput(event) {
+  // console.log(event.target);
+  // console.log(event.target.tagName);
+  $imagePlaceHolder.removeAttribute('src');
+  $imagePlaceHolder.setAttribute('src', $photoURL.value);
+}
+
+$photoURL.addEventListener('input', photoURLInput);
