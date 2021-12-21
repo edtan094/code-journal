caa78071583c0a69700aec3a3cc503577cc9038b
Edwin Tan <etan094@gmail.com>
2021-12-20 16:43:49 -0800

    Listen for 'input' events on the photoUrl input to update the src attribute of the photo preview when the input value changes. Also adjusted css style so that the containers line up.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,13 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+var $imagePlaceHolder = document.querySelector('.image-place-holder');
+var $photoURL = document.querySelector('#photo-URL');
+function photoURLInput(event) {
+  // console.log(event.target);
+  // console.log(event.target.tagName);
+  $imagePlaceHolder.removeAttribute('src');
+  $imagePlaceHolder.setAttribute('src', $photoURL.value);
+}
+
+$photoURL.addEventListener('input', photoURLInput);
