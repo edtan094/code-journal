f93d45189dc0819a5286c664f4f4b7d60bcaf8b5
Edwin Tan <etan094@gmail.com>
2021-12-21 16:31:19 -0800

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML.  Use a loop to create a DOM tree for each journal entry in the data model and append it to the page when the 'DOMContentLoaded' event is fired.

--- js/main.js
+++ js/main.js
@@ -23,5 +23,52 @@ function addingInputIntoObject(event) {
   data.entries.unshift(inputs);
   $imagePlaceHolder.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
+  appendTheDom(data);
 }
 $form.addEventListener('submit', addingInputIntoObject);
+
+function addEntriesToPage(entries) {
+  var $li = document.createElement('li');
+
+  var $divRow = document.createElement('div');
+  $divRow.setAttribute('class', 'row');
+  $li.appendChild($divRow);
+
+  var $divHalfColumn = document.createElement('div');
+  $divHalfColumn.setAttribute('class', 'column-half');
+  $divRow.appendChild($divHalfColumn);
+
+  var $divHalfColumn2 = document.createElement('div');
+  $divHalfColumn2.setAttribute('class', 'column-half');
+  $divRow.appendChild($divHalfColumn2);
+
+  var $divImageContainer = document.createElement('div');
+  $divImageContainer.setAttribute('class', 'image-container');
+  $divHalfColumn.appendChild($divImageContainer);
+
+  var $img = document.createElement('img');
+  $img.setAttribute('class', 'image-size');
+  $img.setAttribute('src', entries.photoURL);
+  $divImageContainer.appendChild($img);
+
+  var $heading = document.createElement('h3');
+  var $headingText = document.createTextNode(entries.title);
+  $heading.appendChild($headingText);
+  $divHalfColumn2.appendChild($heading);
+
+  var $paragraph = document.createElement('p');
+  var $paragraphText = document.createTextNode(entries.notes);
+  $paragraph.appendChild($paragraphText);
+  $divHalfColumn2.appendChild($paragraph);
+
+  return $li;
+
+}
+
+function appendTheDom(event) {
+  var $ul = document.querySelector('.list');
+  for (var i = 0; i < event.entries.length; i++) {
+    var $newEntry = addEntriesToPage(event.entries[i]);
+    $ul.appendChild($newEntry);
+  }
+}
